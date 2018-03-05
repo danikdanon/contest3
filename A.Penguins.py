4f8ddf3ea8792faf015e68fc7211624bdaf677ae
danikdanon <daniilturkenich@mail.ru>
2018-03-05 12:39:10 +0300

    actual versions

--- A.Penguins.py
+++ A.Penguins.py
@@ -1,18 +1,18 @@
 def print_ping(n):
     for i in range(n):
-        print('   _~_   ', end='')
+        print('   _~_    ', end='')
     print()
     for i in range(n):
-        print('  (o o)  ', end='')
+        print('  (o o)   ', end='')
     print()
     for i in range(n):
-        print(' /  V  \ ', end='')
+        print(' /  V  \  ', end='')
     print()
     for i in range(n):
-        print('/(  _  )\\', end='')
+        print('/(  _  )\ ', end='')
     print()
     for i in range(n):
-        print('  ^^ ^^  ', end='')
+        print('  ^^ ^^   ', end='')
     print()
 
 
--- F.Utilites.py
+++ F.Utilites.py
@@ -3,24 +3,23 @@ line_cnt = 0
 symb_cnt = 0
 max_len = 0
 word_cnt = 0
-line = input()
-while line != '#':
-    line_cnt += 1
-    symb_cnt += len(line)
-    max_len = max(max_len, len(line))
-    line1 = line.split(' ')
-    for word in line1:
-        if word != '':
-            word_cnt += 1
-    line = input()
-
-
-commands = commands.split(' ')
-if '-l' in commands:
-    print(line_cnt, end=' ')
-if '-w' in commands:
-    print(word_cnt, end=' ')
-if '-m' in commands:
-    print(symb_cnt, end=' ')
-if '-L' in commands:
-    print(max_len, end=' ')
+try:
+    while 1:
+        line = input()
+        line_cnt += 1
+        symb_cnt += len(line)
+        max_len = max(max_len, len(line))
+        line1 = line.split(' ')
+        for word in line1:
+            if word != '':
+                word_cnt += 1
+except:
+    commands = commands.split(' ')
+    if '-l' in commands:
+        print(line_cnt, end=' ')
+    if '-w' in commands:
+        print(word_cnt, end=' ')
+    if '-m' in commands:
+        print(symb_cnt + line_cnt, end=' ')
+    if '-L' in commands:
+        print(max_len, end=' ')
